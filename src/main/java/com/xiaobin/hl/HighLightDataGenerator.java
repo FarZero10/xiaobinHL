a993e5f0a2eac4590860f64c4fd028aa89d9e451
yuncold <a245876026@126.com>
2024-07-26 23:33:50 +0800

    1.0.0

---
+++ src/main/java/com/xiaobin/hl/HighLightDataGenerator.java
@@ -0,0 +1,11 @@
+package com.xiaobin.hl;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HighLightDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
